58cd370fc7d24d6f37bc3ac012f6367ca140f834
hope2see <ungyo.jung@gmail.com>
2025-03-15 08:42:53 +0900

    update default test setting

--- tests/scripts/test_default.sh
+++ tests/scripts/test_default.sh
@@ -9,17 +9,20 @@ python -u run.py \
     --model TABE \
     --model_id $test_name'_('$desc')' \
     --data TABE_FILE --data_path 'BTC-USD_LogRet_2021-01-01_2023-01-01_1d.csv' \
-    --train_epochs 3  \
-    --basemodel 'EtsModel' \
-    --basemodel 'SarimaModel' \
+    --data_train_splits 0.3 0.5 0.2 \
+    --seq_len 128 --label_len 128 \
+    --train_epochs 10  \
+    --basemodel 'ETS' \
+    --basemodel 'AutoSARIMA' \
     --basemodel 'DLinear --batch_size 8' \
     --basemodel 'iTransformer' \
     --basemodel 'PatchTST --batch_size 16' \
     --basemodel 'TimeXer' \
     --basemodel 'CMamba --batch_size 64 --lradj type3 --learning_rate 0.0005 --d_model 128 --d_ff 128' \
     --basemodel 'TimeMoE' \
-    --combiner '--lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' \
-    --adjuster '--gpm_lookback_win 3 --lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
+    --basemodel 'Timer' \
+    --combiner '--lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
+    # --adjuster '--gpm_lookback_win 3 --lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
 
 
 desc='all_models_SPY'
@@ -28,14 +31,17 @@ python -u run.py \
     --model TABE \
     --model_id $test_name'_('$desc')' \
     --data TABE_FILE --data_path 'SPY_LogRet_2023-01-01_2025-01-01_1d.csv' \
-    --train_epochs 3  \
-    --basemodel 'EtsModel' \
-    --basemodel 'SarimaModel' \
+    --data_train_splits 0.3 0.5 0.2 \
+    --seq_len 128 --label_len 128 \
+    --train_epochs 10  \
+    --basemodel 'ETS' \
+    --basemodel 'AutoSARIMA' \
     --basemodel 'DLinear --batch_size 8' \
     --basemodel 'iTransformer' \
     --basemodel 'PatchTST --batch_size 16' \
     --basemodel 'TimeXer' \
     --basemodel 'CMamba --batch_size 64 --lradj type3 --learning_rate 0.0005 --d_model 128 --d_ff 128' \
     --basemodel 'TimeMoE' \
-    --combiner '--lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' \
-    --adjuster '--gpm_lookback_win 3 --lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
+    --basemodel 'Timer' \
+    --combiner '--lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
+    # --adjuster '--gpm_lookback_win 3 --lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
