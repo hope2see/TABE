1f492b8d7dad49c55cf71248060425d5e90030f7
hope2see <ungyo.jung@gmail.com>
2025-03-16 18:51:04 +0900

    removed clean cmd to avoid using distutils pkg

--- pmdarima/setup.py
+++ pmdarima/setup.py
@@ -11,7 +11,7 @@ import os
 import platform
 import shutil
 
-from distutils.command.clean import clean as Clean
+# from distutils.command.clean import clean as Clean
 from packaging.version import Version
 from setuptools import find_packages
 import traceback
@@ -93,40 +93,41 @@ else:
     extra_setuptools_args = dict()
 
 
-# Custom clean command to remove build artifacts -- adopted from sklearn
-class CleanCommand(Clean):
-    description = "Remove build artifacts from the source tree"
+# # Custom clean command to remove build artifacts -- adopted from sklearn
+# class CleanCommand(Clean):
+#     description = "Remove build artifacts from the source tree"
 
-    # this is mostly in case we ever add a Cython module to SMRT
-    def run(self):
-        Clean.run(self)
-        # Remove c files if we are not within a sdist package
-        cwd = os.path.abspath(os.path.dirname(__file__))
-        remove_c_files = not os.path.exists(os.path.join(cwd, 'PKG-INFO'))
-        if remove_c_files:
-            print('Will remove generated .c files')
-        if os.path.exists('build'):
-            shutil.rmtree('build')
-        for dirpath, dirnames, filenames in os.walk(DISTNAME):
-            for filename in filenames:
-                if any(filename.endswith(suffix) for suffix in
-                       (".so", ".pyd", ".dll", ".pyc")):
-                    print(f'Removing file: {filename}')
-                    os.unlink(os.path.join(dirpath, filename))
-                    continue
-                extension = os.path.splitext(filename)[1]
-                if remove_c_files and extension in ['.c', '.cpp']:
-                    pyx_file = str.replace(filename, extension, '.pyx')
-                    if os.path.exists(os.path.join(dirpath, pyx_file)):
-                        os.unlink(os.path.join(dirpath, filename))
-            for dirname in dirnames:
-                if dirname == '__pycache__':
-                    absdir = os.path.join(dirpath, dirname)
-                    print(f'Removing directory: {absdir}')
-                    shutil.rmtree(absdir)
+#     # this is mostly in case we ever add a Cython module to SMRT
+#     def run(self):
+#         Clean.run(self)
+#         # Remove c files if we are not within a sdist package
+#         cwd = os.path.abspath(os.path.dirname(__file__))
+#         remove_c_files = not os.path.exists(os.path.join(cwd, 'PKG-INFO'))
+#         if remove_c_files:
+#             print('Will remove generated .c files')
+#         if os.path.exists('build'):
+#             shutil.rmtree('build')
+#         for dirpath, dirnames, filenames in os.walk(DISTNAME):
+#             for filename in filenames:
+#                 if any(filename.endswith(suffix) for suffix in
+#                        (".so", ".pyd", ".dll", ".pyc")):
+#                     print(f'Removing file: {filename}')
+#                     os.unlink(os.path.join(dirpath, filename))
+#                     continue
+#                 extension = os.path.splitext(filename)[1]
+#                 if remove_c_files and extension in ['.c', '.cpp']:
+#                     pyx_file = str.replace(filename, extension, '.pyx')
+#                     if os.path.exists(os.path.join(dirpath, pyx_file)):
+#                         os.unlink(os.path.join(dirpath, filename))
+#             for dirname in dirnames:
+#                 if dirname == '__pycache__':
+#                     absdir = os.path.join(dirpath, dirname)
+#                     print(f'Removing directory: {absdir}')
+#                     shutil.rmtree(absdir)
 
 
-cmdclass = {'clean': CleanCommand}
+# cmdclass = {'clean': CleanCommand}
+cmdclass = {}
 
 # build_ext has to be imported after setuptools
 try:
