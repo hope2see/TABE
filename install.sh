5330657288b8b5ba0e7fea9d92ad034b27abcb07
hope2see <ungyo.jung@gmail.com>
2025-03-21 13:49:53 +0900

    update install script

--- install.sh
+++ install.sh
@@ -1,22 +1,61 @@
-# git clone https://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@github.com/hope2see/tabe1.0.git
-# %cd tabe1.0/
-
 #!/bin/bash
 
-# Get the current Python version
-PYTHON_VERSION=$(python --version 2>&1 | awk '{print $2}')
+OS=$(uname -s)
+ARCH=$(uname -m)
 
-# Define the required version
-REQUIRED_VERSION="3.11.11"
+echo "Detected OS: $OS"
+echo "Detected Architecture: $ARCH"
 
-# Install python3.11 if not installed
-if [[ "$PYTHON_VERSION" != "$REQUIRED_VERSION" ]]; then
-    echo "Python version($REQUIRED_VERSION) is required. So, install it ..."
-    sudo apt-get update -y
-    sudo apt-get install python3.11
-    sudo apt install python3.11-dev
-    curl -sS https://bootstrap.pypa.io/get-pip.py | python3.11
+
+# install miniconda -------------------------------------------------
+
+if [[ "$OS" == "Linux" ]]; then
+    if [[ "$ARCH" == "x86_64" ]]; then
+        URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+    elif [[ "$ARCH" == "aarch64" ]]; then
+        URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh"
+    else
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+elif [[ "$OS" == "Darwin" ]]; then  # macOS
+    if [[ "$ARCH" == "x86_64" ]]; then
+        URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh"
+    elif [[ "$ARCH" == "arm64" ]]; then  # Apple M1/M2/M3
+        URL="https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh"
+    else
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+    fi
+else
+    echo "Unsupported OS: $OS"
+    exit 1
 fi
+
+wget -O miniconda.sh "$URL"
+bash Miniconda3-latest-Linux-aarch64.sh
+source ~/miniconda3/bin/activate
+conda init
+
+
+# Install git -----------------------------
+sudo apt update
+sudo apt install git -y
+
+# Install gcc -----------------------------
+sudo apt update
+sudo apt install gcc -y
+
+# Create python env ---------------------
+conda create -n tabe1.0 python=3.11
+conda activate tabe1.0
+
+
+# Install tabe -------------------------------
+
+echo "Downloading Tabe1.0 ..."
+git clone https://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@github.com/hope2see/tabe1.0.git
+cd tabe1.0
 
 echo "Downloading pmdarima ..."
 git clone https://github.com/alkaline-ml/pmdarima.git
@@ -28,18 +67,8 @@ echo "Downloading Time-MoE ..."
 git clone https://github.com/Time-MoE/Time-MoE.git
 
 
-echo "Installing pmdarima ..."
+echo "Installing required packages for pmdarima ..."
 pip install -r ./pmdarima/requirements.txt
-OS=$(uname)
-if [[ "$OS" == "Darwin" ]]; then
-    sed -i '' 's/setuptools>=38.6.0,!=50.0.0/setuptools==58.0.4/' ./pmdarima/requirements.txt
-elif [[ "$OS" == "Linux" ]]; then
-    sed -i 's/setuptools>=38.6.0,!=50.0.0/setuptools==58.0.4/' ./pmdarima/requirements.txt
-else
-    echo "Unsupported OS: $OS"
-fi
-cd pmdarima; python3.11 setup.py develop; python3.11 setup.py install
-
 
 echo "Installing required packages for Time-MoE ..."
 pip install -r ./Time-MoE/requirements.txt
@@ -47,3 +76,7 @@ pip install -r ./Time-MoE/requirements.txt
 echo "Installing required packages for TABE ..."
 pip install -r ./requirements.txt
 
+
+# Build and install pmdarima -------------------------------
+echo "Build & install pmdarima ..."
+cd pmdarima; python3.11 setup.py develop; python3.11 setup.py install
