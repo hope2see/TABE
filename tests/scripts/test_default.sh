802ee29154b7376257c3c6f39db611c03e0485f4
hope2see <ungyo.jung@gmail.com>
2025-03-25 23:57:09 +0900

    .

--- tests/scripts/test_default.sh
+++ tests/scripts/test_default.sh
@@ -26,22 +26,22 @@ python -u tabe_runner.py \
     # --basemodel 'PatchTST --batch_size 16' \
 
 
-desc='all_models_BTC'
+# desc='all_models_BTC'
 
-python -u tabe_runner.py \
-    --model TABE \
-    --model_id $test_name'_('$desc')' \
-    --data TABE_FILE --data_path 'BTC-USD_LogRet_2021-01-01_2023-01-01_1d.csv' \
-    --data_test_split 140 --data_train_splits 0.189 0.625 0.188 \
-    --train_epochs 10  \
-    --seq_len 32 --label_len 32 \
-    --basemodel 'ETS' \
-    --basemodel 'AutoSARIMA' \
-    --basemodel 'DLinear --batch_size 8' \
-    --basemodel 'TimeXer' \
-    --basemodel 'CMamba --batch_size 64 --lradj type3 --learning_rate 0.0005 --d_model 128 --d_ff 128' \
-    --basemodel 'TimeMoE' \
-    --combiner '--lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
-    # --adjuster '--gpm_lookback_win 3 --lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
-    # --basemodel 'iTransformer' \
-    # --basemodel 'PatchTST --batch_size 16' \
+# python -u tabe_runner.py \
+#     --model TABE \
+#     --model_id $test_name'_('$desc')' \
+#     --data TABE_FILE --data_path 'BTC-USD_LogRet_2021-01-01_2023-01-01_1d.csv' \
+#     --data_test_split 140 --data_train_splits 0.189 0.625 0.188 \
+#     --train_epochs 10  \
+#     --seq_len 32 --label_len 32 \
+#     --basemodel 'ETS' \
+#     --basemodel 'AutoSARIMA' \
+#     --basemodel 'DLinear --batch_size 8' \
+#     --basemodel 'TimeXer' \
+#     --basemodel 'CMamba --batch_size 64 --lradj type3 --learning_rate 0.0005 --d_model 128 --d_ff 128' \
+#     --basemodel 'TimeMoE' \
+#     --combiner '--lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
+#     # --adjuster '--gpm_lookback_win 3 --lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
+#     # --basemodel 'iTransformer' \
+#     # --basemodel 'PatchTST --batch_size 16' \
