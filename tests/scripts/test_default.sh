4f202dd945d08583e9ccce5630972164f3b21fca
hope2see <ungyo.jung@gmail.com>
2025-03-25 23:56:52 +0900

    .

--- tests/scripts/test_default.sh
+++ tests/scripts/test_default.sh
@@ -9,8 +9,8 @@ python -u tabe_runner.py \
     --model TABE \
     --model_id $test_name'_('$desc')' \
     --data TABE_FILE --data_path 'TSLA_LogRet_2020-01-01_2025-03-01_1d.csv' \
-    --data_test_split 252 --data_train_splits 0.43 0.5 0.07 \
-    --seq_len 32 --label_len 32 \
+    --data_test_split 252 --data_train_splits 0.45 0.5 0.05 \
+    --seq_len 128 --label_len 128 \
     --train_epochs 10  \
     --basemodel 'ETS' \
     --basemodel 'AutoSARIMA' \
@@ -32,17 +32,15 @@ python -u tabe_runner.py \
     --model TABE \
     --model_id $test_name'_('$desc')' \
     --data TABE_FILE --data_path 'BTC-USD_LogRet_2021-01-01_2023-01-01_1d.csv' \
-    --data TABE_FILE --data_path BTC-USD_LogRet_2021-01-01_2023-01-01_1d.csv \
     --data_test_split 140 --data_train_splits 0.189 0.625 0.188 \
     --train_epochs 10  \
+    --seq_len 32 --label_len 32 \
     --basemodel 'ETS' \
     --basemodel 'AutoSARIMA' \
     --basemodel 'DLinear --batch_size 8' \
     --basemodel 'TimeXer' \
     --basemodel 'CMamba --batch_size 64 --lradj type3 --learning_rate 0.0005 --d_model 128 --d_ff 128' \
     --basemodel 'TimeMoE' \
-    --basemodel 'Timer' \
-    --basemodel 'TimesFM' \
     --combiner '--lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
     # --adjuster '--gpm_lookback_win 3 --lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' 
     # --basemodel 'iTransformer' \
